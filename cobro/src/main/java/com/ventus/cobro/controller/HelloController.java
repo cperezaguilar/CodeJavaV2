ef12212580f3fef68f11b7d2bbf6e5342a07fdba
carlos.perez <carlos.perez@ventus-tech.com>
2023-08-28 11:25:59 -0600

    Primer commit de prueba nexus

---
+++ cobro/src/main/java/com/ventus/cobro/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.ventus.cobro.controller;
+
+import org.apache.commons.text.StringSubstitutor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    Logger logger = LogManager.getLogger(HelloController.class);
+    @RequestMapping("/")
+    String hellow()
+    {
+        StringSubstitutor interpolator = StringSubstitutor.createInterpolator();
+        String text = interpolator.replace(
+                "Base64 Decoder:        ${base64Decoder:SGVsbG9Xb3JsZCE=}\n" +
+                        "Base64 Encoder:        ${base64Encoder:HelloWorld!}\n");
+
+        logger.info(text);
+        logger.info("Dentro de la app");
+        return "Hello World!";
+    }
+}
